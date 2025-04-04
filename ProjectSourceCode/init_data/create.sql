1c80cdc21810330f46833094381c0cff336e6069
Cole Clare <coleridge.clare@colorado.edu>
2025-04-04 14:44:22 -0400

    changed db volumes, edit create.sql for syntax stuff, redirected login to reviews, all of this to make database work for me

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY NOT NULL,
-  username VARCHAR(50) UNIQUE KEY NOT NULL,
-  email VARCHAR(50) UNIQUE KEY NOT NULL,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  email VARCHAR(50) UNIQUE NOT NULL,
   password VARCHAR(60) NOT NULL
 );
 
@@ -13,7 +13,7 @@ CREATE TABLE trails (
   location VARCHAR(100),
   avg_rating DECIMAL,
   image INT,
-  description VARCHAR(500) NOT NULL,
+  description VARCHAR(500) NOT NULL
 );
 
 DROP TABLE IF EXISTS reviews;
@@ -29,12 +29,12 @@ CREATE TABLE reviews (
 
 DROP TABLE IF EXISTS trails_to_reviews;
 CREATE TABLE trails_to_reviews (
-    trail_id NOT NULL,
-    review_id NOT NULL
+    trail_id INT NOT NULL, -- needed a data type, threw in int for now
+    review_id INT NOT NULL -- needed a data type, threw in int for now
 );
 
 DROP TABLE IF EXISTS users_to_reviews;
 CREATE TABLE users_to_reviews (
-    user_id NOT NULL,
-    review_id NOT NULL,
+    user_id INT NOT NULL, -- needed a data type, threw in int for now
+    review_id INT NOT NULL -- needed a data type, threw in int for now
 );
