d488d070d5acd565ab53d1c6bfee3f4c6d71c7b4
XavierRudnick <xavierrudnick@yahoo.com>
2025-04-24 12:56:24 -0600

    update external url for init_db from render

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -4,7 +4,7 @@
 # This file contains sensitive information and should be kept private
 
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="postgresql://users_db_mkhf_user:tmwnYtCbOOAh2Ww7qiyA4mvVhD7LZvMu@dpg-d00k1j49c44c73fognv0-a.oregon-postgres.render.com/users_db_mkhf"
+PG_URI="postgresql://users_db_7x3v_user:jxmVX1XnWFMTvL6ciZPnt2kZBJDDJ7zf@dpg-d057s7a4d50c73ahqurg-a.oregon-postgres.render.com/users_db_7x3v"
 
 # Execute each .sql file in the directory
 for file in init_data/*.sql; do
